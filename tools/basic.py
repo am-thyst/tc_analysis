20d4cec079591412a529a5a807af2a874fbfafef
Amethyst <amefishjohnson@gmail.com>
2021-11-21 10:31:33 +0000

    Add calc for CFL

--- tools/basic.py
+++ tools/basic.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import iris
 
 def hrs_since_init(year_0,month_0,day_0,hr_0,year_1,month_1,day_1,hr_1):
     # Calculates time since model initialisation in hrs
@@ -7,7 +8,15 @@ def hrs_since_init(year_0,month_0,day_0,hr_0,year_1,month_1,day_1,hr_1):
     end = str(day_1) + '/' + str(month_1) + '/' + str(year_1) + ' ' + str(hr_1) +':00'
     FMT = '%d/%m/%Y %H:%M'
     tdelta = datetime.strptime(end, FMT) - datetime.strptime(start, FMT)
-    day_hours = tdelta.days * 24
     second_hours = tdelta.seconds / 60 / 60
     return(int(day_hours+second_hours))
 
+def CFL(ws,ts=120,res=4400,Iris=False):
+    # Calculates CFL number. > 1 indicates smaller ts needed.
+    # Can be used on xarray, numpy array, iris, int or float
+    if iris == False:
+        cfl = ws * (ts / res)
+    if Iris == True:
+        tr = ts / res
+        cfl = iris.analysis.maths.multiply(ws,tr)    
+    return(cfl)
